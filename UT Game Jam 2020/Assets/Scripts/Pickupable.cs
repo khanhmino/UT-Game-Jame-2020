b5f96cd2f6c52e4918415898aba805de57b78052
Alexhwa <ah.alswad@gmail.com>
2020-09-17 21:21:40 -0500

    Setup

---
+++ UT Game Jam 2020/Assets/Scripts/Pickupable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Pickupable : ScriptableObject
+{
+    //How fast the sword can be swung with this blade
+    public float swingSpeed = 1;
+    //How many hits the blade can do without breaking
+    public float durability = 1;
+    //How much damage this blade does to enemies
+    public float damage = 1;
+}
---
+++ UT Game Jam 2020/Assets/Scripts/SwordController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    public GameObject sword;
+    public float pickupRange;
+    public LayerMask pickupLayers;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PointToMouse();
+        LookForPickups();
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwingBlade();
+        }
+    }
+
+    private GameObject[] LookForPickups()
+    {
+        Collider2D[] foundPickups = Physics2D.OverlapCircleAll(transform.position, pickupRange, pickupLayers);
+        GameObject[] pickupables = new GameObject[foundPickups.Length];
+        for(int i = 0; i < pickupables.Length; i++)
+        {
+            print("Found pickup " + i);
+            pickupables[i] = foundPickups[i].gameObject;
+        }
+        return pickupables;
+    }
+
+    private void PointToMouse()
+    {
+        Vector3 mousePos;
+        Vector3 objPos;
+
+
+        mousePos = Input.mousePosition;
+        mousePos.z = 10; //The distance between the camera and object
+        objPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objPos.x;
+        mousePos.y = mousePos.y - objPos.y;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+
+    public void PickUpBlade(PickupController pickup)
+    {
+
+    }
+
+    public void DiscardBlade()
+    {
+
+    }
+    public void SwingBlade()
+    {
+        anim.SetTrigger("Swinging");
+    }
+}
